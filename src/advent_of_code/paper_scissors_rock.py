9b1a231623d9dd1abeb42de947fc511684c141db
lyndsayp <lyndsayp@gmail.com>
2022-12-05 00:00:28 +0100

    Single round tests pass

--- src/advent_of_code/paper_scissors_rock.py
+++ src/advent_of_code/paper_scissors_rock.py
@@ -1,2 +1,21 @@
 def scoreOneRound(round):
-    return 8
+    # A/X Rock, B/Y Paper, C/Z Scissors
+    plays = {'A':1, 'B':2, 'C':3, 'X':1, 'Y':2, 'Z':3}
+
+    opponentPlay = plays[round[0]]
+    myPlay = plays[round[1]]
+
+    win = myPlay > opponentPlay
+    loose = myPlay < opponentPlay
+    draw = myPlay == opponentPlay
+
+    roundScore = myPlay
+
+    if loose :
+        roundScore += 0
+    elif draw :
+        roundScore += 3
+    elif win :
+        roundScore += 6
+
+    return roundScore
--- test/test_day_2.py
+++ test/test_day_2.py
@@ -1,6 +1,11 @@
 from advent_of_code.paper_scissors_rock import *
 
-def test_score_one_round():
+def test_score_rock_paper_win():
     score = scoreOneRound(('A', 'Y'))
 
     assert score == 8
+
+def test_score_paper_rock_loose():
+    score = scoreOneRound(('B', 'X'))
+
+    assert score == 1
