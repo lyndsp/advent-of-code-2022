392eb803f6d6e1aab98458dbfa0cdae8d48c166a
lyndsayp <lyndsayp@gmail.com>
2022-12-07 17:38:31 +0000

    Day 3-2 done!

--- src/advent_of_code/rucksack_compartments.py
+++ src/advent_of_code/rucksack_compartments.py
@@ -35,3 +35,38 @@ def totalMatchingItemsPriority(path):
             totalPriority += findMatchingItemPriority(items)
     
     return totalPriority
+
+def findCommonItem(rucksacks) :
+    itemsInFirstTwoRucksacks = {i for i in rucksacks[0] if i in rucksacks[1]}
+
+    itemsInThreeRucksacks = {i for i in itemsInFirstTwoRucksacks if i in rucksacks[2]}
+
+    return itemsInThreeRucksacks.pop()
+
+def splitIntoGroupsOf3(path):
+    groups = list()
+    singleGroup = list()
+
+    with open(path) as f:
+        for line in f:
+            items = line.strip()
+            singleGroup.append(items)
+
+            if len(singleGroup) == 3 :
+                groups.append(singleGroup.copy())
+                singleGroup.clear()
+    
+    return groups
+
+
+def commonItemsTotalPriority(path):
+    groups = splitIntoGroupsOf3(path)
+
+    priorities = list()
+
+    for group in groups :
+        item = findCommonItem(group)
+        priority = itemPriority(item)
+        priorities.append(priority)
+    
+    return sum(priorities)
--- test/test_day_3.py
+++ test/test_day_3.py
@@ -40,3 +40,40 @@ def test_day_3_1():
     totalPriority = totalMatchingItemsPriority("day-3.txt")
 
     assert totalPriority == 7908
+
+def test_find_common_item_in_rucksacks():
+    rucksacks = [
+        list("vJrwpWtwJgWrhcsFMMfFFhFp"),
+        list("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"), 
+        list("PmmdzqPrVvPwwTWBwg")]
+
+    commonItem = findCommonItem(rucksacks)
+
+    assert commonItem == 'r'
+
+def test_find_common_item_in_more_rucksacks():
+    rucksacks = [
+        list("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"),
+        list("ttgJtRGJQctTZtZT"), 
+        list("CrZsJsPPZsGzwwsLwLmpwMDw")]
+
+    commonItem = findCommonItem(rucksacks)
+
+    assert commonItem == 'Z'
+
+def test_split_into_groups_of_3():
+    groups = splitIntoGroupsOf3("day-3-test.txt")
+
+    assert len(groups) == 2
+
+def test_find_common_items_total_priority():
+
+    totalPriority = commonItemsTotalPriority("day-3-test.txt")
+
+    assert totalPriority == 70
+
+def test_day_3_2():
+    totalPriority = commonItemsTotalPriority("day-3.txt")
+
+    assert totalPriority == 2838
+
