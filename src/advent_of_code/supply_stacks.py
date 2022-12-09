489cfa4c5cbc516330ed5929dec027aec70b11da
lyndsayp <lyndsayp@gmail.com>
2022-12-09 18:18:53 +0000

    Day 5 part 1 solved!

---
+++ src/advent_of_code/supply_stacks.py
@@ -0,0 +1,54 @@
+class Stack:
+    def __init__(self, definition):
+        self.stack = list(definition)
+
+    def add(self, items):
+
+        for i in items:
+            self.stack.append(i)
+
+    def top(self):
+        return str(self.stack[len(self.stack)-1])
+
+    def move(self, itemCount):
+        movedItems = list()
+
+        itemCounter = 0
+
+        while itemCounter != itemCount:
+            movedItems.append(self.stack.pop())
+            itemCounter += 1
+        
+        return movedItems
+
+
+class Stacks:
+    def __init__(self, definition):
+        self.stacks = list()
+
+        for s in definition:
+            self.stacks.append(s)
+    
+    # "move 1 from 1 to 2"
+    def action(self, commandText):
+        command = Command(commandText)
+        movedItems = self.stacks[command.fromStack].move(command.moveItems)
+
+        self.stacks[command.toStack].add(movedItems)
+
+    def top(self):
+        topItems = list()
+        
+        for s in self.stacks :
+            topItems.append(s.top())
+
+        return "".join(topItems)
+
+class Command:
+    def __init__(self, definition):
+        components = definition.split()
+
+        self.moveItems = int(components[1])
+        self.fromStack = int(components[3]) - 1
+        self.toStack = int(components[5]) - 1
+
---
+++ test/test_day_5.py
@@ -0,0 +1,60 @@
+from advent_of_code.supply_stacks import *
+
+# F <- Top
+# G
+# R
+# J
+# L
+# D <- Bottom
+  
+def test_move_one_item():
+    stack = Stack("DLJRVGF")
+
+    assert stack.move(1) == list("F")
+
+def test_move_two_items():
+    stack = Stack("DLJRVGF")
+
+    assert stack.move(2) == list("FG")
+
+def test_multiple_stacks():
+    stacks = list([Stack("DLJRVGF"), Stack("TPMBVHJS")])
+
+    assert len(stacks) == 2
+
+# move 3 from 4 to 6
+# move 1 from 5 to 8
+
+def test_parse_command():
+    command = Command("move 1 from 5 to 8")
+
+    assert command.fromStack == 5
+    assert command.toStack == 8
+    assert command.moveItems == 1
+
+def test_move_item_from_one_stack_to_another():
+    stacks = Stacks([Stack("DLJRVGF"), Stack("TPMBVHJS")])
+
+    stacks.action("move 1 from 1 to 2")
+
+    assert stacks.top() == "GF"
+
+def test_day_5_1():
+    stacks = Stacks([
+        Stack("DLJRVGF"), 
+        Stack("TPMBVHJS"),
+        Stack("VHMFDGPC"),
+        Stack("MDPNGQ"),
+        Stack("FNHLJ"),
+        Stack("NFVQDHTZ"),
+        Stack("FDBL"),
+        Stack("MJBSVDN"),
+        Stack("GLD"),
+        ])
+
+    with open("day-5.txt") as f:
+        for line in f:
+            stacks.action(line.strip())
+    
+    assert stacks.top() == "QMBMJDFTD"
+
